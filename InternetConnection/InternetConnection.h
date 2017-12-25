b3e9240695a71b4a3e3f3ebfd869da768f780c3e
kerimcaglar <kerimcaglar@macbook-pro.local>
2017-12-25 22:10:35 +0300

    Initial Commit

---
+++ InternetConnection/InternetConnection.h
@@ -0,0 +1,19 @@
+//
+//  InternetConnection.h
+//  InternetConnection
+//
+//  Created by kerimcaglar on 25/12/2017.
+//  Copyright © 2017 kerimcaglar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InternetConnection.
+FOUNDATION_EXPORT double InternetConnectionVersionNumber;
+
+//! Project version string for InternetConnection.
+FOUNDATION_EXPORT const unsigned char InternetConnectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InternetConnection/PublicHeader.h>
+
+
